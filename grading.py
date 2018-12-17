add3c9e200d5f2f5b90b22ededa0af02004a640e
fuurin <komatsuemgbr0624@gmail.com>
2018-12-17 16:21:28 +0900

    文字化けエラーに対応

--- grading.py
+++ grading.py
@@ -1,4 +1,8 @@
-import sys, os, subprocess, glob, re
+import sys
+import os
+import subprocess
+import glob
+import re
 import pandas as pd
 from argparse import ArgumentParser
 
@@ -22,23 +26,24 @@ args = parser.parse_args()
 # Constants
 ID_LIST = pd.read_csv("Participant.csv")["ID"]
 DIR = args.dir_name if args.dir_name.endswith('/') else args.dir_name + '/'
-COMPILER = "gcc" # c compiler, gcc, clang ...
-TIMEOUT_PERIOD = 2 # second
-COMMENT_THD = args.t # required comment threshold
-WITH_COMMENT = args.w # add comment column in RESULT_FILE
+COMPILER = "gcc"  # c compiler, gcc, clang ...
+TIMEOUT_PERIOD = 2  # second
+COMMENT_THD = args.t  # required comment threshold
+WITH_COMMENT = args.w  # add comment column in RESULT_FILE
 A_COMMENT = "OKです．"
 B_COMMENT = "もう少しコメントを書きましょう．"
 C_COMMENT = "プログラムが間違っています．"
 C_NOT_MATCH_COMMENT = "出力が間違っています．"
 D_COMMENT = "未提出です．"
-SCHECK = args.s # source check, execution check
+SCHECK = args.s  # source check, execution check
 RESULT_FILE = "grading.csv"
-OUTPUT_PATTERN = args.o.replace('\\n', '\n') # expected output pattern
+OUTPUT_PATTERN = args.o.replace('\\n', '\n')  # expected output pattern
 CONLY = args.c
 if args.i is "":
     STDIN = ""
 else:
     STDIN = "echo %s | " % args.i
+
 
 def get_color(grade):
     # return color with ANSI escape code
@@ -54,10 +59,11 @@ def get_color(grade):
         # RED
         return "\033[31m", "\033[0m"
     elif grade is "D":
-        #GREEN
+        # GREEN
         return "\033[32m", "\033[0m"
     else:
         return "UNDEFINED", "GRADE"
+
 
 if __name__ == "__main__":
     # seiseki
@@ -66,12 +72,12 @@ if __name__ == "__main__":
 
     # Main Loop
     for i, id_ in enumerate(ID_LIST):
-        num_comment, num_line = 0, 0 # Number of lines, Number of comment lines
+        num_comment, num_line = 0, 0  # Number of lines, Number of comment lines
         if len(glob.glob(DIR+id_+"/*.c")) != 0:
             # submitted
-            c_file = glob.glob(DIR+id_+"/*.c")[0] # c source
-            output = c_file.split(".c")[0] # output file
-            
+            c_file = glob.glob(DIR+id_+"/*.c")[0]  # c source
+            output = c_file.split(".c")[0]  # output file
+
             # compile *.c
             cerror = subprocess.run(
                 [COMPILER, c_file, "-lm", "-w", "-o", output],
@@ -81,9 +87,9 @@ if __name__ == "__main__":
 
             # execution
             runcmd = "%s%s" % (STDIN, output)
-            
+
             print(" ===== STDOUT =====")
-            print("%s: \n" % id_) # print on terminal
+            print("%s: \n" % id_)  # print on terminal
 
             if SCHECK:
                 print(" ===== SOURCE CODE ===== ")
@@ -93,9 +99,8 @@ if __name__ == "__main__":
                             sys.stdout.write(line)
                 except Exception as error:
                     print(error)
-                
-                print()
 
+                print()
 
             if cerror.returncode is 0:
                 # succeed compile
@@ -105,19 +110,18 @@ if __name__ == "__main__":
                     if line != b"\n":
                         num_line += 1
 
-                if args.i: 
+                if args.i:
                     print(" ===== INPUT ===== ")
                     print("%s\n" % args.i)
 
                 timeout = False
                 try:
-                    exe = subprocess.run([runcmd], shell=True, 
-                                     stdout=subprocess.PIPE,
-                                     stderr=subprocess.PIPE,
-                                     timeout=TIMEOUT_PERIOD)
+                    exe = subprocess.run([runcmd], shell=True,
+                                         stdout=subprocess.PIPE,
+                                         stderr=subprocess.PIPE,
+                                         timeout=TIMEOUT_PERIOD)
                 except subprocess.TimeoutExpired as timeout_error:
                     timeout = True
-                    
 
                 # check output
                 if not timeout:
@@ -128,50 +132,54 @@ if __name__ == "__main__":
                 # grading
                 if exe.returncode >= 0 and not timeout:
                     if OUTPUT_PATTERN:
-                        if re.search(OUTPUT_PATTERN, result_str): 
-                            srtc ,endc = get_color("D") # Green
-                            match = "\n%s%s%s\n" % (srtc, "Match!!!" ,endc)
-                            
+                        if re.search(OUTPUT_PATTERN, result_str):
+                            srtc, endc = get_color("D")  # Green
+                            match = "\n%s%s%s\n" % (srtc, "Match!!!", endc)
+
                             if num_comment >= num_line*COMMENT_THD or (COMMENT_THD > 1 and num_comment >= COMMENT_THD):
-                                grade[i] = "A" # good code
+                                grade[i] = "A"  # good code
                                 comment[i] = A_COMMENT
                             else:
-                                grade[i] = "B" # not enough comment
-                                comment[i] = B_COMMENT    
+                                grade[i] = "B"  # not enough comment
+                                comment[i] = B_COMMENT
                         else:
-                            srtc ,endc = get_color("C") # Red
-                            match = "%s%s%s\n" % (srtc, "Mismatch..." ,endc)
-                            grade[i] = "C" # not enough comment
-                            comment[i] = C_NOT_MATCH_COMMENT    
+                            srtc, endc = get_color("C")  # Red
+                            match = "%s%s%s\n" % (srtc, "Mismatch...", endc)
+                            grade[i] = "C"  # not enough comment
+                            comment[i] = C_NOT_MATCH_COMMENT
                         print(match)
                     else:
                         if num_comment >= num_line*COMMENT_THD or (COMMENT_THD > 1 and num_comment >= COMMENT_THD):
-                            grade[i] = "A" # good code
+                            grade[i] = "A"  # good code
                             comment[i] = A_COMMENT
                         else:
-                            grade[i] = "B" # not enough comment
+                            grade[i] = "B"  # not enough comment
                             comment[i] = B_COMMENT
                 else:
-                    grade[i] = "C" # execution error
+                    grade[i] = "C"  # execution error
                     comment[i] = C_COMMENT
                     if timeout:
                         print(" ===== TIME OUT ===== \n")
                     else:
                         print(" ===== ERROR ===== ")
-                        print(cerror.stderr.decode('utf-8'))                    
+                        print(cerror.stderr.decode('utf-8'))
             else:
-                grade[i] = "C" # compile error
+                grade[i] = "C"  # compile error
                 comment[i] = C_COMMENT
                 print(" ===== ERROR ===== ")
-                print(cerror.stderr.decode('utf-8'))
+                try:
+                    print(cerror.stderr.decode('utf-8'))
+                except UnicodeDecodeError as ex:
+                    print("文字化けが発生しています")
         else:
-            grade[i] = "D" # not submitted
+            grade[i] = "D"  # not submitted
             comment[i] = D_COMMENT
 
         # print grade
-        srtc ,endc = get_color(grade[i])
+        srtc, endc = get_color(grade[i])
         print(" ===== GRADE ===== ")
-        msg = "%s%s: GRADE = %s, comment/line = %d/%d%s" % (srtc, id_, grade[i], num_comment, num_line, endc)
+        msg = "%s%s: GRADE = %s, comment/line = %d/%d%s" % (
+            srtc, id_, grade[i], num_comment, num_line, endc)
         print("%s\n\n\n\n\n" % msg)
 
     if CONLY:
@@ -181,12 +189,15 @@ if __name__ == "__main__":
         dir_out = DIR.split("/")[-2]
         if os.path.exists(RESULT_FILE):
             df = pd.read_csv(RESULT_FILE)
-            df = pd.concat([df, pd.DataFrame(grade, columns=[dir_out])], axis=1)
-            if WITH_COMMENT: df = pd.concat([df, pd.DataFrame(comment, columns=[dir_out+"comment"])], axis=1)
+            df = pd.concat(
+                [df, pd.DataFrame(grade, columns=[dir_out])], axis=1)
+            if WITH_COMMENT:
+                df = pd.concat(
+                    [df, pd.DataFrame(comment, columns=[dir_out+"Comment"])], axis=1)
             df.to_csv(RESULT_FILE, index=False)
         else:
             df = pd.DataFrame({
                 "ID": ID_LIST,
-                dir_out : grade
+                dir_out: grade
             }, columns=["ID", dir_out])
             df.to_csv(RESULT_FILE, index=False)
